6f66e48a7bccc5f4491f9754089f955fc9a9bede
Marcus Geiger <github0e@antbear.org>
2014-07-08 08:54:20 +0200

    Fixed unused parameters warning.

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -228,7 +228,7 @@ resubmissionarControllers.controller('CreateCustomerCtrl', ['$scope', '$location
                     file: file      // or list of files: $files for html5 only
                 }).progress(function (evt) {
                     console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-                }).success(function (data, status, headers, config) { // file is uploaded successfully
+                }).success(function (data /*, status, headers, config*/) { // file is uploaded successfully
                     console.log(data);
                     $scope.uploads = data; // store uploads info in the scope to be used when customer is created
                     if (data != null && data.length > 0) {
